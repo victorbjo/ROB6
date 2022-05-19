f89e832fe1caf64ff3326365eab7ff78e5bc36ea
jimmi <jijens19@student.aau.dk>
2022-05-19 12:44:51 +0200

    map_gang

--- map.py
+++ map.py
@@ -6,12 +6,12 @@ from geometry_msgs.msg import PoseStamped
 import matplotlib.pyplot as plt
 from matplotlib import image
 
-img = image.imread("/home/jimmi/catkin_ws/src/mir_robot/ROB6/Lab1.png")
+#img = image.imread("/home/jimmi/catkin_ws/src/mir_robot/ROB6/Lab1.png")
 
-print(img)
+#print(img)
 
-plt.plot(img,"aa")
-plt.show()
+#plt.plot(img,"aa")
+#plt.show()
 
 pub = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10)
 rospy.init_node("move")
@@ -25,17 +25,6 @@ rospy.init_node("move")
 import numpy as np # Scientific computing library for Python
  
 def get_quaternion_from_euler(roll, pitch, yaw):
-  """
-  Convert an Euler angle to a quaternion.
-   
-  Input
-    :param roll: The roll (rotation around x-axis) angle in radians.
-    :param pitch: The pitch (rotation around y-axis) angle in radians.
-    :param yaw: The yaw (rotation around z-axis) angle in radians.
- 
-  Output
-    :return qx, qy, qz, qw: The orientation in quaternion [x,y,z,w] format
-  """
     qx = np.sin(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) - np.cos(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
     qy = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
     qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
@@ -69,14 +58,16 @@ def goTo(x,y, z_rot):
     pub.publish(goal)
 
 
-a = (108*0.05, (636-238)*0.05)
+a = (810*0.05, (636-150)*0.05)
+
 b = (210*0.05, (636-412)*0.05)
 c = (145*0.05, (636-308)*0.05)
 d = (153*0.05, (636-261)*0.05)
 
-#goTo(5.140, 19.728, 0.0,
-#     0.0, 0.0, -0.475, 0.880)
 
-#rospy.spin()
+
+goTo(810*0.05, (636-150)*0.05, 1.57*3)
+
+rospy.spin()
 
 
