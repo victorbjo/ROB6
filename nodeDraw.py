db6c885645b1cdee3b92b3f0b06be38bb74d511b
victorbjo <victor.bjoerholm@gmail.com>
2022-05-22 14:42:34 +0200

    Cleaned up

--- nodeDraw.py
+++ nodeDraw.py
@@ -3,7 +3,7 @@ import json
 import numpy as np
 #image = cv2.imread("images/RunKinAndAngles.png")
 #image = cv2.imread("images/RunNoAngularConstraints.png")
-filename = "BedAndRobot.png"
+filename = "hallwayNoKinematic1.png"
 image = cv2.imread(filename)
 shapes = np.zeros_like(image, np.uint8)
 radius = 20
@@ -12,11 +12,11 @@ config = open("config.json")
 config = json.load(config)
 color = (30, 30, 255)
 color2 = (80, 80, 80)
-point0 = 1
-point1 = 0
-image = cv2.circle(image, (220,60), 4, color, -1)
-image = cv2.circle(image, (50,380), 4, color, -1)
-cv2.circle(shapes, (50,380), 20, color2, 2)
+point0 = (805,105)
+point1 = (650,422)
+image = cv2.circle(image, point0, 4, color, -1)
+image = cv2.circle(image, point1, 4, color, -1)
+cv2.circle(shapes, point1, 20, color2, 2)
 out = image.copy()
 alpha = 0.5
 mask = shapes.astype(bool)
